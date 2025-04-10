e3d9c1bb60d723160a6a0fcb33d48169be3cf7c1
deepraj_chouhan <deepraj.chouhan@iflair.com>
2025-04-10 19:47:40 +0530

    again push

--- src/Component/Aboutus/AboutUS.tsx
+++ src/Component/Aboutus/AboutUS.tsx
@@ -13,6 +13,9 @@ interface AboutDataType {
     happyclient:string
     imageone:string
     imagetwo:string
+    headingtwo:string
+    headingtwodescriiption:string
+    imagethree:string
     }
 }
 const About = () => {
@@ -20,7 +23,7 @@ const About = () => {
     const datas= data.aboutus
     return (
         <>
-        <section className="py-44 relative">
+        <section className="py-44  relative">
         <div className="w-full max-w-7xl px-4 md:px-5 lg:px-5 mx-auto">
             <div className="w-full justify-start items-center gap-12 grid lg:grid-cols-2 grid-cols-1">
                 <div
@@ -58,7 +61,22 @@ const About = () => {
                 </div>
             </div>
         </div>
+        </section>
+
+        <section className="py-14 relative">
+        <div className="w-full max-w-7xl px-4 md:px-5 lg:px-5 mx-auto">
+            <div className="w-full justify-start items-center gap-8 grid lg:grid-cols-2 grid-cols-1">
+                <div className="w-full flex-col justify-start lg:items-start items-center gap-10 inline-flex">
+                    <div className="w-full flex-col justify-start lg:items-start items-center gap-4 flex">
+                        <h2 className="text-gray-900 text-4xl font-bold font-manrope leading-normal lg:text-start text-center">{datas.headingtwo}</h2>
+                        <p className="text-gray-500 text-base font-normal leading-relaxed lg:text-start text-center">{datas.headingtwodescriiption}</p>
+                    </div>
+                </div>
+                <img className="lg:mx-0 mx-auto h-full rounded-3xl object-cover" src={datas.imagethree} alt="about Us image" />
+            </div>
+        </div>
     </section>
+                                            
                                             
         </>
     )
