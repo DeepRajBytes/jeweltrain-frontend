801e1ac3fedee693d3096947c07989f280ed9311
deepraj_chouhan <deepraj.chouhan@iflair.com>
2025-04-10 13:17:59 +0530

    commit some changes

--- src/Component/Home.tsx
+++ src/Component/Home.tsx
@@ -1,11 +1,13 @@
 import Hero from './Hero'
 import Services from './Services/Services';
+import Team from './myteam/Team';
 const Home = () => {
 return (
 <>
   <Hero>
   </Hero>
   <Services></Services>
+  <Team></Team>
 </>
 )
 }
