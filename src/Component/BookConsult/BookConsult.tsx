74d7d1d72b54bb3845956ad87142c24cf730505f
deepraj_chouhan <deepraj.chouhan@iflair.com>
2025-04-11 18:08:58 +0530

    code commited

--- src/Component/BookConsult/BookConsult.tsx
+++ src/Component/BookConsult/BookConsult.tsx
@@ -7,7 +7,9 @@ import { ChevronDownIcon } from '@heroicons/react/16/solid'
 interface Bookconsultant {
   headingone:string,
   headingtwo:string,
-  submitButton:string
+  submitButton:string,
+  thankyou:string,
+  thankyoumesage:string
 }
 
 const BookConsult = () => {
@@ -172,7 +174,8 @@ const BookConsult = () => {
            {data.submitButton}
           </button>
         </div>
-      </form>): (<div className="flex items-center justify-center h-80">
+      </form>): (
+      <div className="flex items-center justify-center h-80">
         <div className="p-4 rounded shadow-lg ">
           <div className="flex flex-col items-center space-y-2">
             <svg xmlns="http://www.w3.org/2000/svg" className="text-green-600 w-28 h-28" fill="none" viewBox="0 0 24 24"
@@ -180,11 +183,11 @@ const BookConsult = () => {
                 <path stroke-linecap="round" stroke-linejoin="round"
                     d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
-            <h1 className="text-4xl font-bold">Thank You !</h1>
-            <p>Thank you for your interest! Our Executive Connect Over a Call or Email Soon.</p>
+            <h1 className="text-4xl font-bold">{data.thankyou}</h1>
+            <p>{data.thankyoumesage}</p>
           </div>
         </div>
-        </div>)}
+      </div>)}
     </div>
   )
 }
--- src/Component/Career/Career.tsx
+++ src/Component/Career/Career.tsx
@@ -1,19 +1,365 @@
-const links = [
-  { name: 'Open roles', href: '#' },
-  { name: 'Internship program', href: '#' },
-  { name: 'Our values', href: '#' },
-  { name: 'Meet our leadership', href: '#' },
-]
-const stats = [
-  { name: 'Offices worldwide', value: '12' },
-  { name: 'Full-time colleagues', value: '300+' },
-  { name: 'Hours per week', value: '40' },
-  { name: 'Paid time off', value: 'Unlimited' },
-]
+import { ChevronDownIcon, DocumentArrowUpIcon } from "@heroicons/react/24/outline"
+import CareerPage from "../../assets/content/content.json"
+// const links = [
+//   { name: 'Open roles', href: '#' },
+//   { name: 'Internship program', href: '#' },
+//   { name: 'Our values', href: '#' },
+//   { name: 'Meet our leadership', href: '#' },
+// ]
+// const stats = [
+//   { name: 'Offices worldwide', value: '12' },
+//   { name: 'Full-time colleagues', value: '300+' },
+//   { name: 'Hours per week', value: '40' },
+//   { name: 'Paid time off', value: 'Unlimited' },
+// ]
+
+interface FormDetailType {
+  submitbutton: string,
+  formhead: string,
+  formheaddesc: string
+}
+
+interface CareerPageType {
+  CareerPage : {
+    Brandname:string,
+    brandlogolink:string,
+    CEONAME:string,
+    Messagefromceo:string,
+    position:string,
+    personimg:string,
+    FormDetail:FormDetailType
+  }
+} 
 
 const Career = () => {
-  return (
-    <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
+  const data: CareerPageType = CareerPage
+  const datas = data.CareerPage.FormDetail
+  // const handleSubmit = () => {
+  //   console.log("clicked");
+  // }
+   return (
+   <div className="font-mono">
+  {/* CEO section */}
+   <section className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,var(--color-indigo-100),white)] opacity-20" />
+      <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl ring-1 shadow-indigo-600/10 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
+      <div className="mx-auto max-w-2xl lg:max-w-4xl">
+        {/* <img
+          alt=""
+          src="https://tailwindcss.com/plus-assets/img/logos/workcation-logo-indigo-600.svg"
+          className="mx-auto h-12"
+        /> */}
+        <figure className="mt-10">
+          <blockquote className="text-center text-xl/8 font-semibold text-gray-900 sm:text-2xl/9">
+            <p>
+            {data.CareerPage.Messagefromceo}
+            </p>
+          </blockquote>
+          <figcaption className="mt-10">
+            <img
+              alt=""
+              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              className="mx-auto size-10 rounded-full"
+            />
+            <div className="mt-4 flex items-center justify-center space-x-3 text-base">
+              <div className="font-semibold text-gray-900">{data.CareerPage.CEONAME}</div>
+              <svg width={3} height={3} viewBox="0 0 2 2" aria-hidden="true" className="fill-gray-900">
+                <circle r={1} cx={1} cy={1} />
+              </svg>
+              <div className="text-gray-600">{data.CareerPage.position}</div>
+            </div>
+          </figcaption>
+        </figure>
+      </div>
+    </section>
+
+  {/* Forms Section */}
+  <div className="flex justify-center pb-10 px-4 sm:px-6 lg:px-8">
+    <form>
+      <div className="space-y-12">
+        <div className="border-b border-gray-900/10 pb-12">
+          <h2 className="text-base/7 font-semibold text-gray-900">{datas.formhead}</h2>
+          <p className="mt-1 text-sm/6 text-gray-600">
+           {datas.formheaddesc}
+          </p>
+
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+
+            <div className="col-span-full">
+              <label htmlFor="about" className="block text-sm/6 font-medium text-gray-900">
+                About
+              </label>
+              <div className="mt-2">
+                <textarea
+                  id="about"
+                  name="about"
+                  rows={3}
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  defaultValue={''}
+                />
+              </div>
+              <p className="mt-3 text-sm/6 text-gray-600">Write a few sentences about yourself.</p>
+            </div>
+
+          {/* resume section */}
+          <div className="col-span-full">
+          <label htmlFor="cover-photo" className="block text-sm/6 font-medium text-gray-900">Your Resume</label>
+          <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+            <div className="text-center">
+              <DocumentArrowUpIcon aria-hidden="true" className="mx-auto size-12 text-gray-300" />
+              <div className="mt-4 flex text-sm/6 text-gray-600">
+                <label
+                  htmlFor="file-upload"
+                  className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                >
+                  <span>Upload a file</span>
+                  <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                </label>
+                <p className="pl-1">or drag and drop</p>
+              </div>
+              <p className="text-xs/5 text-gray-600">PDF or DOCx up to 10MB</p>
+            </div>
+          </div>
+        </div>
+          </div>
+        </div>
+
+          {/* personal information section  */}
+        <div className="border-b border-gray-900/10 pb-12">
+          <h2 className="text-base/7 font-semibold text-gray-900">Personal Information</h2>
+          <p className="mt-1 text-sm/6 text-gray-600">Use a permanent address where you can receive mail.</p>
+
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-3">
+              <label htmlFor="first-name" className="block text-sm/6 font-medium text-gray-900">
+                First name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="first-name"
+                  name="first-name"
+                  type="text"
+                  autoComplete="given-name"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-3">
+              <label htmlFor="last-name" className="block text-sm/6 font-medium text-gray-900">
+                Last name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="last-name"
+                  name="last-name"
+                  type="text"
+                  autoComplete="family-name"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-4">
+              <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-4">
+              <label htmlFor="number" className="block text-sm/6 font-medium text-gray-900">
+                Number
+              </label>
+              <div className="mt-2">
+                <input
+                  id="number"
+                  name="number"
+                  type="number"
+                  autoComplete="number"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-3">
+              <label htmlFor="country" className="block text-sm/6 font-medium text-gray-900">
+                Country
+              </label>
+              <div className="mt-2 grid grid-cols-1">
+                <select
+                  id="country"
+                  name="country"
+                  autoComplete="country-name"
+                  className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                >
+                  <option>India</option>
+                  <option>Nepal</option>
+                </select>
+                <ChevronDownIcon
+                  aria-hidden="true"
+                  className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
+                />
+              </div>
+            </div>
+
+            <div className="col-span-full">
+              <label htmlFor="street-address" className="block text-sm/6 font-medium text-gray-900">
+                Street address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="street-address"
+                  name="street-address"
+                  type="text"
+                  autoComplete="street-address"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-2 sm:col-start-1">
+              <label htmlFor="city" className="block text-sm/6 font-medium text-gray-900">
+                City
+              </label>
+              <div className="mt-2">
+                <input
+                  id="city"
+                  name="city"
+                  type="text"
+                  autoComplete="address-level2"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-2">
+              <label htmlFor="region" className="block text-sm/6 font-medium text-gray-900">
+                State / Province
+              </label>
+              <div className="mt-2">
+                <input
+                  id="region"
+                  name="region"
+                  type="text"
+                  autoComplete="address-level1"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-2">
+              <label htmlFor="total-expirence" className="block text-sm/6 font-medium text-gray-900">
+                Total Year of expirence
+              </label>
+              <div className="mt-2">
+                <input
+                  id="total-expirence"
+                  name="total-expirence"
+                  type="number"
+                  autoComplete="total-expirence"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-4">
+              <label htmlFor="current-company" className="block text-sm/6 font-medium text-gray-900">
+               Current Orgination
+              </label>
+              <div className="mt-2">
+                <input
+                  id="current-company"
+                  name="current-company"
+                  type="text"
+                  autoComplete="current-company"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+              <div className="sm:col-span-2 sm:col-start-1">
+              <label htmlFor="currentCTC" className="block text-sm/6 font-medium text-gray-900">
+                Current CTC
+              </label>
+              <div className="mt-2">
+                <input
+                  id="currentCTC"
+                  name="currentCTC"
+                  type="text"
+                  autoComplete="address-level2"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-2">
+              <label htmlFor="expectedCtc" className="block text-sm/6 font-medium text-gray-900">
+                Expected CTC
+              </label>
+              <div className="mt-2">
+                <input
+                  id="expectedCtc"
+                  name="expectedCtc"
+                  type="text"
+                  autoComplete="expectedCtc"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label htmlFor="relocate" className="block text-sm/6 font-medium text-gray-900">
+                Ready to Relocate
+              </label>
+               <div className="mt-2 flex space-x-4">
+            <label className="inline-flex items-center">
+              <input type="radio" name="relocate" value="yes" className="mr-2" />
+              Yes
+            </label>
+            <label className="inline-flex items-center">
+              <input type="radio" name="relocate" value="no" className="mr-2" />
+              No
+            </label>
+          </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-6 flex items-center justify-end gap-x-6">
+        <button type="button" className="text-sm/6 font-semibold text-gray-900">
+          Cancel
+        </button>
+        <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Save
+        </button>
+      </div>
+    </form>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+    {/* <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
       <img
         alt=""
         src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
@@ -69,6 +415,7 @@ const Career = () => {
           </dl>
         </div>
       </div>
+    </div> */}
     </div>
   )
 }
